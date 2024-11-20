399a1c684386ff32f0d8f91d1c971ab06d34f29c
MDSHADAB72 <128897805+MDSHADAB72@users.noreply.github.com>
2024-11-21 00:08:39 +0530

    Delete Md-Shadab-Spring-Boot-Project-Management/src directory

--- Md-Shadab-Spring-Boot-Project-Management/src/main/java/com/SHadab/project_tracker/EntityConverter/EntityConverter.java
+++
@@ -1,7 +0,0 @@
-package com.mihneacristian.project_tracker.EntityConverter;
-
-public interface EntityConverter<ENTITY, DTO> {
-
-    public DTO convertToDTO(ENTITY entity);
-    public ENTITY convertToEntity (DTO dto);
-}
